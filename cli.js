282a47b17856a1983e6b4b6b64cf588e8a58a285
Sergio Cinos <sergio.cinos@gmail.com>
2018-10-31 15:53:06 +1100

    Fix typo in option description

--- cli.js
+++ cli.js
@@ -11,7 +11,7 @@ commander
     .usage('[options] <yarn.lock path>')
     .option('-s, --strategy <strategy>', 'deduplication strategy. Valid values: fewer, highest. Default is "highest"', /^(fewer|highest)$/i, 'highest')
     .option('-l, --list', 'do not change yarn.lock, just output the diagnosis')
-    .option('--packages <packages>', 'a comma separated list of packages to dedupe. Defauls to all packages.', val => val.split(',').map(v => v.trim()))
+    .option('--packages <packages>', 'a comma separated list of packages to dedupe. Defaults to all packages.', val => val.split(',').map(v => v.trim()))
     .option('--print', 'instead of saving the deduped yarn.lock, print the result in stdout')
 commander.parse(process.argv);
 if (!commander.args.length) commander.help();
