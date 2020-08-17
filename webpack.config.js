4684a0445b5ee2bc70d3e8fd981208bb4ac1e435
Thach Nguyen <nthachus@gmail.com>
2020-08-17 21:10:34 +0700

    - Fix the webpack config to make 2 bundles working correctly

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,22 @@ const TerserPlugin = require('terser-webpack-plugin');
 const CopyPkgJsonPlugin = require('copy-pkg-json-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+const baseConfig = (name) => ({
     mode: 'production',
-    entry: {
-        cli: './cli.js',
-        index: './index.js',
-    },
+    entry: './' + name,
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
+        filename: name + '.js',
     },
+    name,
     context: __dirname,
     target: 'node',
     node: {
         __filename: false,
         __dirname: false,
     },
-    externals: {
-        './index': 'commonjs ./index',
-    },
     module: {
         rules: [
-            {
-                test: path.resolve(__dirname, 'cli.js'),
-                loader: 'string-replace-loader',
-                options: { search: '^#!.*[\\r\\n]+', flags: '', replace: '' },
-            },
             {
                 // transpile ES6-8 into ES5
                 test: /\.m?js$/,
@@ -40,21 +30,8 @@ module.exports = {
                     presets: [['@babel/preset-env', { targets: { node: '6' } }]], // esmodules
                 },
             },
-            {
-                test: path.resolve(__dirname, 'package.json'),
-                loader: 'string-replace-loader',
-                options: { search: ',\\s*"bin":.*$', flags: 's', replace: '}' },
-            },
         ],
     },
-    plugins: [
-        new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true, test: /cli/ }),
-        new CopyPkgJsonPlugin({
-            remove: ['dependencies', 'devDependencies', 'scripts', 'engines', 'eslintConfig'],
-            replace: { engines: { node: '>=6' } },
-        }),
-        new CopyPlugin([{ from: '*.{txt,md,ts}' }]),
-    ],
     optimization: {
         nodeEnv: false,
         // minimize: false,
@@ -69,4 +46,38 @@ module.exports = {
             }),
         ],
     },
-};
+});
+
+const config = [baseConfig('index'), baseConfig('cli')];
+
+Object.assign(config[0].output, {
+    libraryTarget: 'commonjs2',
+});
+
+Object.assign(config[1], {
+    externals: {
+        './index': 'commonjs2 ./index',
+    },
+    plugins: [
+        new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+        new CopyPkgJsonPlugin({
+            remove: ['dependencies', 'devDependencies', 'scripts', 'engines', 'eslintConfig'],
+            replace: { engines: { node: '>=6' } },
+        }),
+        new CopyPlugin([{ from: '*.{txt,md,ts}' }]),
+    ],
+});
+config[1].module.rules.unshift(
+    {
+        test: path.resolve(__dirname, 'cli.js'),
+        loader: 'string-replace-loader',
+        options: { search: '^#!.*[\\r\\n]+', flags: '', replace: '' },
+    },
+    {
+        test: path.resolve(__dirname, 'package.json'),
+        loader: 'string-replace-loader',
+        options: { search: ',\\s*"bin":.*$', flags: 's', replace: '}' },
+    }
+);
+
+module.exports = config;
