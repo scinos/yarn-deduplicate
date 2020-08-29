44ccb078050c7d8ddbec5ffebc3de2fd07b8d12b
Sergio Cinos <sergio.cinos@gmail.com>
2020-08-29 12:24:54 +0200

    Use variadic support from new commander

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -11,175 +11,231 @@ const cliFilePath = path.join(__dirname, '../cli.js');
 const fixturesPath = path.join(__dirname, '../__fixtures__');
 const yarnLockFilePath = path.join(fixturesPath, 'yarn.lock');
 
-test('prints duplicates', async () => {
-    const { stdout, stderr } = await execFile(process.execPath, [
-        cliFilePath,
-        '--list',
-        yarnLockFilePath,
-    ]);
-    expect(stdout).toMatch(/^.+\n.+\n$/);
-    expect(stderr).toBe('');
-});
-
-test('fails if given the fail option', async () => {
-    const listProc = await childProcess.spawn(process.execPath, [
-        cliFilePath,
-        '--list',
-        '--fail',
-        yarnLockFilePath,
-    ]);
-    listProc.on('close', (code) => {
-        expect(code).toBe(1);
-    });
-
-    const execProc = await childProcess.spawn(process.execPath, [
-        cliFilePath,
-        '--print',
-        '--fail',
-        yarnLockFilePath,
-    ]);
-    execProc.on('close', (code) => {
-        expect(code).toBe(1);
-    });
-});
-
-test('does not fail without the fail option', async () => {
-    const listProc = await childProcess.spawn(process.execPath, [
-        cliFilePath,
-        '--list',
-        yarnLockFilePath,
-    ]);
-    listProc.on('close', (code) => {
-        expect(code).toBe(0);
-    });
-
-    const execProc = await childProcess.spawn(process.execPath, [
-        cliFilePath,
-        '--print',
-        yarnLockFilePath,
-    ]);
-    execProc.on('close', (code) => {
-        expect(code).toBe(0);
-    });
-});
-
-test('prints fixed yarn.lock', async () => {
+const testWithFlags = async (flags) => {
     const { stdout, stderr } = await execFile(process.execPath, [
         cliFilePath,
         '--print',
+        ...flags,
+        '--',
         yarnLockFilePath,
     ]);
-    expect(stdout).not.toContain('"@scope/lib@>=1.0.0":');
-    expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-    expect(stdout).not.toContain('lodash@>=1.0.0:');
-    expect(stdout).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
     expect(stderr).toBe('');
-});
+    return stdout;
+};
 
-test('prints fixed yarn.lock when listing lodash package', async () => {
-    const { stdout, stderr } = await execFile(process.execPath, [
-        cliFilePath,
-        '--print',
-        '--packages',
-        'lodash',
-        yarnLockFilePath,
-    ]);
-    expect(stdout).not.toContain('lodash@>=1.0.0:');
-    expect(stdout).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
-    expect(stderr).toBe('');
-});
-
-test('prints fixed yarn.lock when listing @scope scope', async () => {
-    const { stdout, stderr } = await execFile(process.execPath, [
-        cliFilePath,
-        '--print',
-        '--scopes',
-        '@scope',
-        yarnLockFilePath,
-    ]);
-    expect(stdout).not.toContain('"@scope/lib@>=1.0.0":');
-    expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-    expect(stderr).toBe('');
-});
-
-test('prints same yarn.lock when listing missing package', async () => {
-    const { stdout, stderr } = await execFile(process.execPath, [
-        cliFilePath,
-        '--print',
-        '--packages',
-        'foo',
-        yarnLockFilePath,
-    ]);
-    expect(stdout).toContain('"@scope/lib@>=1.0.0":');
-    expect(stdout).not.toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-    expect(stdout).toContain('lodash@>=1.0.0:');
-    expect(stdout).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
-    expect(stderr).toBe('');
-});
-
-test('prints fixed yarn.lock when excluding lodash', async () => {
-    const { stdout, stderr } = await execFile(process.execPath, [
-        cliFilePath,
-        '--print',
-        '--exclude',
-        'lodash',
-        yarnLockFilePath,
-    ]);
-    expect(stdout).not.toContain('"@scope/lib@>=1.0.0":');
-    expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-    expect(stdout).toContain('lodash@>=1.0.0:');
-    expect(stdout).toContain('lodash@>=2.0.0:');
-    expect(stdout).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
-    expect(stderr).toBe('');
-});
-
-test('edits yarn.lock and replaces its content with the fixed version', async () => {
-    const oldFileContent = await readFile(yarnLockFilePath, 'utf8');
-    try {
+describe('Basic output', () => {
+    test('prints duplicates', async () => {
         const { stdout, stderr } = await execFile(process.execPath, [
             cliFilePath,
+            '--list',
             yarnLockFilePath,
         ]);
-        const newFileContent = await readFile(yarnLockFilePath, 'utf8');
-        expect(oldFileContent).not.toBe(newFileContent);
-        expect(oldFileContent).toContain('"@scope/lib@>=1.0.0":');
-        expect(oldFileContent).not.toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-        expect(oldFileContent).toContain('lodash@>=1.0.0:');
-        expect(oldFileContent).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
-
-        expect(newFileContent).not.toContain('"@scope/lib@>=1.0.0":');
-        expect(newFileContent).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-        expect(newFileContent).not.toContain('lodash@>=1.0.0:');
-        expect(newFileContent).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
-        expect(stdout).toBe('');
+        expect(stdout).toContain(
+            'Package "@scope/lib" wants >=1.0.0 and could get 4.17.15, but got 1.3.1'
+        );
+        expect(stdout).toContain(
+            'Package "@another-scope/lib" wants >=1.0.0 and could get 4.17.15, but got 1.3.1'
+        );
+        expect(stdout).toContain(
+            'Package "lodash" wants >=1.0.0 and could get 4.17.15, but got 1.3.1'
+        );
         expect(stderr).toBe('');
-    } finally {
-        await writeFile(yarnLockFilePath, oldFileContent, 'utf8');
-    }
+    });
+
+    test('prints fixed yarn.lock', async () => {
+        const { stdout, stderr } = await execFile(process.execPath, [
+            cliFilePath,
+            '--print',
+            yarnLockFilePath,
+        ]);
+        expect(stdout).not.toContain('"@scope/lib@>=1.0.0":');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+        expect(stdout).not.toContain('lodash@>=1.0.0:');
+        expect(stdout).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+        expect(stderr).toBe('');
+    });
 });
 
-test('edits yarn.lock and replaces its content with the fixed version without specifying yarn.lock path', async () => {
-    const oldFileContent = await readFile(yarnLockFilePath, 'utf8');
-    try {
-        const { stdout, stderr } = await execFile(process.execPath, [cliFilePath], {
-            cwd: fixturesPath,
-        });
-        const newFileContent = await readFile(yarnLockFilePath, 'utf8');
-        expect(oldFileContent).not.toBe(newFileContent);
-        expect(oldFileContent).toContain('"@scope/lib@>=1.0.0":');
-        expect(oldFileContent).not.toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-        expect(oldFileContent).toContain('lodash@>=1.0.0:');
-        expect(oldFileContent).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+describe('Edit in place', () => {
+    test('edits yarn.lock and replaces its content with the fixed version', async () => {
+        const oldFileContent = await readFile(yarnLockFilePath, 'utf8');
+        try {
+            const { stdout, stderr } = await execFile(process.execPath, [
+                cliFilePath,
+                yarnLockFilePath,
+            ]);
+            const newFileContent = await readFile(yarnLockFilePath, 'utf8');
+            expect(oldFileContent).not.toBe(newFileContent);
+            expect(oldFileContent).toContain('"@scope/lib@>=1.0.0":');
+            expect(oldFileContent).not.toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+            expect(oldFileContent).toContain('lodash@>=1.0.0:');
+            expect(oldFileContent).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
 
-        expect(newFileContent).not.toContain('"@scope/lib@>=1.0.0":');
-        expect(newFileContent).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
-        expect(newFileContent).not.toContain('lodash@>=1.0.0:');
-        expect(newFileContent).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
-        expect(stdout).toBe('');
-        expect(stderr).toBe('');
-    } finally {
-        await writeFile(yarnLockFilePath, oldFileContent, 'utf8');
-    }
+            expect(newFileContent).not.toContain('"@scope/lib@>=1.0.0":');
+            expect(newFileContent).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+            expect(newFileContent).not.toContain('lodash@>=1.0.0:');
+            expect(newFileContent).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+            expect(stdout).toBe('');
+            expect(stderr).toBe('');
+        } finally {
+            await writeFile(yarnLockFilePath, oldFileContent, 'utf8');
+        }
+    });
+
+    test('edits yarn.lock and replaces its content with the fixed version without specifying yarn.lock path', async () => {
+        const oldFileContent = await readFile(yarnLockFilePath, 'utf8');
+        try {
+            const { stdout, stderr } = await execFile(process.execPath, [cliFilePath], {
+                cwd: fixturesPath,
+            });
+            const newFileContent = await readFile(yarnLockFilePath, 'utf8');
+            expect(oldFileContent).not.toBe(newFileContent);
+            expect(oldFileContent).toContain('"@scope/lib@>=1.0.0":');
+            expect(oldFileContent).not.toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+            expect(oldFileContent).toContain('lodash@>=1.0.0:');
+            expect(oldFileContent).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+
+            expect(newFileContent).not.toContain('"@scope/lib@>=1.0.0":');
+            expect(newFileContent).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+            expect(newFileContent).not.toContain('lodash@>=1.0.0:');
+            expect(newFileContent).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+            expect(stdout).toBe('');
+            expect(stderr).toBe('');
+        } finally {
+            await writeFile(yarnLockFilePath, oldFileContent, 'utf8');
+        }
+    });
+});
+
+describe('Error control', () => {
+    test('fails if given the fail option', async () => {
+        const listProc = await childProcess.spawn(process.execPath, [
+            cliFilePath,
+            '--list',
+            '--fail',
+            yarnLockFilePath,
+        ]);
+        listProc.on('close', (code) => {
+            expect(code).toBe(1);
+        });
+
+        const execProc = await childProcess.spawn(process.execPath, [
+            cliFilePath,
+            '--print',
+            '--fail',
+            yarnLockFilePath,
+        ]);
+        execProc.on('close', (code) => {
+            expect(code).toBe(1);
+        });
+    });
+
+    test('does not fail without the fail option', async () => {
+        const listProc = await childProcess.spawn(process.execPath, [
+            cliFilePath,
+            '--list',
+            yarnLockFilePath,
+        ]);
+        listProc.on('close', (code) => {
+            expect(code).toBe(0);
+        });
+
+        const execProc = await childProcess.spawn(process.execPath, [
+            cliFilePath,
+            '--print',
+            yarnLockFilePath,
+        ]);
+        execProc.on('close', (code) => {
+            expect(code).toBe(0);
+        });
+    });
+});
+
+describe('Supports individal packages', () => {
+    test('it accepts a single package', async () => {
+        const stdout = await testWithFlags(['--packages', 'lodash']);
+        expect(stdout).not.toContain('lodash@>=1.0.0:');
+        expect(stdout).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+    });
+
+    test('it accepts a multiple packages in one flag', async () => {
+        const stdout = await testWithFlags(['--packages', 'lodash', '@scope/lib']);
+        expect(stdout).not.toContain('lodash@>=1.0.0:');
+        expect(stdout).not.toContain('@scope/lib@>=1.0.0:');
+        expect(stdout).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+    });
+
+    test('it accepts a multiple flags', async () => {
+        const stdout = await testWithFlags(['--packages', 'lodash', '--packages', '@scope/lib']);
+        expect(stdout).not.toContain('lodash@>=1.0.0:');
+        expect(stdout).not.toContain('@scope/lib@>=1.0.0:');
+        expect(stdout).toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+    });
+
+    // eslint-disable-next-line jest/expect-expect
+    test('does not break if package is missing', () => testWithFlags(['--packages', 'foo']));
+});
+
+describe('Supports scopes', () => {
+    test('it accepts a single scope', async () => {
+        const stdout = await testWithFlags(['--scopes', '@scope']);
+        expect(stdout).not.toContain('"@scope/lib@>=1.0.0":');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+    });
+
+    test('it accepts a multiple scopes in one flag', async () => {
+        const stdout = await testWithFlags(['--scopes', '@scope', '@another-scope']);
+        expect(stdout).not.toContain('"@scope/lib@>=1.0.0":');
+        expect(stdout).not.toContain('"@another-scope/lib@>=1.0.0":');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+        expect(stdout).toContain('"@another-scope/lib@>=1.0.0", "@another-scope/lib@>=2.0.0":');
+    });
+
+    test('it accepts a multiple flags', async () => {
+        const stdout = await testWithFlags(['--scopes', '@scope', '--scopes', '@another-scope']);
+        expect(stdout).not.toContain('"@scope/lib@>=1.0.0":');
+        expect(stdout).not.toContain('"@another-scope/lib@>=1.0.0":');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+        expect(stdout).toContain('"@another-scope/lib@>=1.0.0", "@another-scope/lib@>=2.0.0":');
+    });
+
+    // eslint-disable-next-line jest/expect-expect
+    test('does not break if scope is missing', () => testWithFlags(['--scopes', '@foo']));
+});
+
+describe('Supports excluding packages', () => {
+    test('it accepts a single package', async () => {
+        const stdout = await testWithFlags(['--exclude', 'lodash']);
+        expect(stdout).toContain('lodash@>=1.0.0:');
+        expect(stdout).toContain('lodash@>=2.0.0:');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+        expect(stdout).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+    });
+
+    test('it accepts a multiple package in one flag', async () => {
+        const stdout = await testWithFlags(['--exclude', 'lodash', '@scope/lib']);
+        expect(stdout).toContain('lodash@>=1.0.0:');
+        expect(stdout).toContain('lodash@>=2.0.0:');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0":');
+        expect(stdout).toContain('"@scope/lib@>=2.0.0":');
+        expect(stdout).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+        expect(stdout).not.toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+    });
+
+    test('it accepts a multiple flags', async () => {
+        const stdout = await testWithFlags(['--exclude', 'lodash', '--exclude', '@scope/lib']);
+        expect(stdout).toContain('lodash@>=1.0.0:');
+        expect(stdout).toContain('lodash@>=2.0.0:');
+        expect(stdout).toContain('"@scope/lib@>=1.0.0":');
+        expect(stdout).toContain('"@scope/lib@>=2.0.0":');
+        expect(stdout).not.toContain('lodash@>=1.0.0, lodash@>=2.0.0:');
+        expect(stdout).not.toContain('"@scope/lib@>=1.0.0", "@scope/lib@>=2.0.0":');
+    });
+
+    // eslint-disable-next-line jest/expect-expect
+    test('does not break if scope is missing', () => testWithFlags(['--exclude', '@foo']));
 });
 
 test('line endings are retained', async () => {
