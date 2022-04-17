ee977d445dece0d57435f2ccfbb0ca638919115a
Sergio Cinos <sergio.cinos@gmail.com>
2022-04-17 09:56:52 +0200

    Initial migration to Typescript

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function listDuplicates(yarnLock: string, { includePackages, useMostCommon }: {
-    includePackages?: string[];
-    useMostCommon?: boolean;
-}): string[];
-export declare function fixDuplicates(yarnLock: string, { includePackages, useMostCommon }?: {
-    includePackages?: string[];
-    useMostCommon?: boolean;
-}): string;
